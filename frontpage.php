6b849fd4232d127573cd4422c2ae92b1421dce32
Marcus Klausen <marcusklausen@Marcus-MBP.local>
2021-02-01 12:21:15 +0100

    added project listing to the front page

--- frontpage.php
+++ frontpage.php
@@ -1,22 +1,13 @@
 <main>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac convallis mi, sit amet sollicitudin metus. Proin a sem sapien. Donec sed lectus sed ligula imperdiet egestas eu id erat. Sed eget nisi sit amet risus dictum efficitur. Phasellus nibh risus, malesuada a luctus vitae, scelerisque a turpis. Donec auctor varius orci in euismod. Nam rhoncus enim quis velit vehicula, eu posuere felis efficitur. Sed venenatis sagittis interdum. Nunc ut mauris dictum, vestibulum massa at, malesuada lorem.
-
-Proin libero ligula, rutrum sed venenatis sit amet, placerat finibus risus. In luctus ultricies lectus, sed dictum elit sodales vel. Nam non elementum nisi. Etiam sit amet purus ut felis faucibus hendrerit et eu ante. Fusce sit amet tincidunt nulla, et iaculis augue. Sed id porttitor lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis porttitor leo vel orci convallis, id venenatis lorem rutrum. Aliquam a commodo lectus. Donec nec elit ut libero consectetur aliquet non eget orci. Proin eu mauris magna. In hac habitasse platea dictumst.
-
-Proin sit amet tempor ex, eu ullamcorper magna. Donec eget turpis ac dolor scelerisque finibus in a arcu. Quisque commodo lobortis massa quis rutrum. Vivamus fringilla elementum dictum. Sed vel pellentesque augue. Praesent aliquet, ex sed imperdiet molestie, dui ligula ultricies tortor, in imperdiet justo massa quis metus. Sed non erat ultricies diam varius tincidunt eu at massa. Aenean pharetra pellentesque tincidunt. Aliquam erat volutpat. Quisque congue massa libero, et consectetur quam fermentum sed. Cras bibendum dapibus interdum. Maecenas leo ipsum, tristique in auctor sit amet, dictum a quam.
-
-Vestibulum facilisis eleifend magna, at euismod diam tristique vitae. Etiam blandit ac diam quis posuere. Nam fermentum neque eget lorem congue, at vehicula nunc laoreet. Nullam sit amet lacus vitae lorem auctor varius ac posuere magna. In convallis, enim non auctor rutrum, diam erat feugiat tortor, sed placerat mauris dui sit amet mauris. Phasellus velit urna, dignissim a commodo sed, placerat ac arcu. Mauris lacinia accumsan varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend consectetur felis, ut mollis lorem placerat non. Donec facilisis sem in nisi sagittis tincidunt. Aliquam erat volutpat.
-    </p>
-    <section class="jumbotron">
-        <h1>Hvem er Marcus?</h1>
-    <p>
-        En ambitiøs frontend udvikler, der er opslugt af programmering og design.</p>
-    <p>For tiden er han elev på Digital Media uddannelsen i Roskilde.
-    </p>
+    <h3>Seneste projekter</h3>
+    <section class="project">
+        <img src="img/thumbs/thumb2.png" alt="Preview" class="thumb"> <!-- skal laves mindre filstørrelse -->
+        <h4>Min Portfolio</h4>
+        <p>Næste opgave på Digital Media GF2, var at lave egen portfolio side. Det svært at blive tilfreds med resultatet, ud fra hvad jeg har lært indtil videre. Men jeg gør mit bedste.ˇ</p>
     </section>
-    <div class="skills">
-
-        
-    </div>
+    <section class="project">
+        <img src="img/thumbs/mockup.png" alt="Preview" class="thumb"> <!-- skal laves mindre filstørrelse -->
+        <h4>Arnes Portfolio</h4>
+        <p>Første project på Digital Media GF2, var en fiktiv portfolio case for Arne Nougatgren, fra Jul på Vesterbro.</p>
+    </section>
 </main>
