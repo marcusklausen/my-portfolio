1a116d54ffebf38c23501bd06d04a333d4e7917b
Marcus Klausen <marcusklausen@Marcus-MBP.local>
2021-02-01 11:40:37 +0100

    initial commit

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Marcus' porfolio</title>
+</head>
+
+<body>
+    <div class="bg frontpage">
+    <header>
+        <div class="wrapper">
+        <nav>
+            <ul class="flex-right">
+                <a href="index.php"><li class="nav-btn-blue">Forside</li></a>
+                <a href="portfolio.php"><li class="nav-btn-green-inverse">Portfolio</li></a>
+                <a href="contact.php"><li class="nav-btn-green-inverse">Kontakt</li></a>
+            </ul>
+        </nav>
+        <div id="tagline">
+            <h1>Marcus Klausen</h1>
+            <h2>En ambitiøs frontend udvikler, der er opslugt af programmering og grafisk design.</h2>
+            <button>Send mig en email</button>
+        </div><!-- tagline -->
+        </div><!-- wrapper -->
+    </header>
+    </div><!-- bg -->
+    <div class="wrapper spacing flex-center">
---
+++ sidebar.php
@@ -0,0 +1,32 @@
+<aside>
+    <h3>Tidslinje</h3>
+    <div id="timeline">
+        <div class="title">
+            <div class="point">
+            </div>
+            <div class="date">Januar 2021</div>
+            Startede på Digital Media GF2 uddannelsen.
+        </div>
+        <div class="title">
+            <div class="point">
+                <div class="line"></div>
+            </div>
+            <div class="date">Oktober 2015</div>
+            Fik stilling som Servicetekniker hos Humac.
+        </div>
+        <div class="title">
+            <div class="point">
+                <div class="line"></div>
+            </div>
+            <div class="date">April 2014</div>
+            Fik stilling som Webdesigner hos DanaWeb.
+        </div>
+        <div class="title">
+            <div class="point">
+                <div class="line"></div>
+            </div>
+            <div class="date">August 2011</div>
+            Fik stilling som Webdesigner hos The Copy Shop.
+        </div>
+    </div>
+</aside>
