d99951b373868c19833914a817d22ab6092f6e3b
Marcus Klausen <marcusklausen@Marcus-MBP.local>
2021-02-15 22:15:14 +0100

    fixed bug where height would dissappar due to no content in div

--- frontpage.php
+++ frontpage.php
@@ -2,8 +2,6 @@
 
     <h3>Seneste projekter</h3>
     <div class="flex spacing ma-center">
-
-
         <div class="card portfolio">
             <div class="img" style="background: url('img/thumbs/mockup.png');
                                     background-position: top center;
@@ -14,17 +12,29 @@
                 <button>Se mere</button>
                 <button>Github</button>
             </div>
-        </div>
-        <div class="card">
-            <div class="icon curious"></div><!-- skal laves mindre filstørrelse -->
-            <h4>Lærenem</h4>
-            <p>Elsker at lære nye ting, specielt når det kommer til webudvikling. Værdsætter absolut folk som kan vise mig forbedrede teknologier, processer med videre.</p>
-        </div>
-        <div class="card">
-            <div class="icon idea"></div><!-- skal laves mindre filstørrelse -->
-            <h4>Kreativ</h4>
-            <p>Er god til, og ikke bleg for at komme med idéer og forslag, hvis jeg ser en mulighed for, at noget kan forbedres.</p>
-        </div>
+        </div><!-- card -->
+        <div class="card portfolio">
+            <div class="img" style="background: url('img/thumbs/thumb2.png');
+                                    background-position: top center;
+                                    background-repeat: no-repeat;
+                                    background-size: cover;"></div>
+            <div class="caption">
+                <h4>Arnes Portfolio</h4>
+                <button>Se mere</button>
+                <button>Github</button>
+            </div>
+        </div><!-- card -->
+        <div class="card portfolio">
+            <div class="img" style="background: url('img/thumbs/theband.png');
+                                    background-position: top center;
+                                    background-repeat: no-repeat;
+                                    background-size: cover;"></div>
+            <div class="caption">
+                <h4>Arnes Portfolio</h4>
+                <button>Se mere</button>
+                <button>Github</button>
+            </div>
+        </div><!-- card -->
     </div>
     <h3>Det jeg er bedst til</h3>
 
