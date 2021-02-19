a6f74cbab16724a54c298d0b6ec3a8ed6b82f4e3
Marcus Klausen <marcusklausen@Marcus-MBP.local>
2021-02-19 21:05:51 +0100

    added paging logic

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php require_once('header.php'); ?>
-<?php require_once('frontpage.php'); ?>
+<?php 
+
+if(isset($_GET['p'])) {
+
+    $p = $_GET['p'];
+
+    switch ($p) {
+        case 'work' : require_once('work.php');
+        break;
+        case 'home' : require_once('frontpage.php');
+        break;
+    }
+} else { require_once('frontpage.php'); }
+
+
+?>
 <?php require_once('sidebar.php'); ?>
 <?php require_once('footer.php'); ?>
